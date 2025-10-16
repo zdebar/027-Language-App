6bc1752f7036481230bf03189daccf4fbcc04264
zdebar <157598603+zdebar@users.noreply.github.com>
2025-10-16 16:01:26 +0200

    feat: add audioFiles constant with various audio file imports

---
+++ constants/audio-files.ts
@@ -0,0 +1,14 @@
+const audioFiles = {
+  english: require("@/assets/audio/english.mp3"),
+  yes: require("@/assets/audio/yes.mp3"),
+  no: require("@/assets/audio/no.mp3"),
+  boy: require("@/assets/audio/boy.mp3"),
+  girl: require("@/assets/audio/girl.mp3"),
+  brother: require("@/assets/audio/brother.mp3"),
+  sister: require("@/assets/audio/sister.mp3"),
+  mother: require("@/assets/audio/mother.mp3"),
+  father: require("@/assets/audio/father.mp3"),
+  dog: require("@/assets/audio/dog.mp3"),
+};
+
+export default audioFiles;
